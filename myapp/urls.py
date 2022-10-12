aa78af61aa8786d64d8d6aa31dbac6b0e3b4be9a
ElCANC3R <diejoruiz1993@gmail.com>
2022-10-12 00:17:56 -0500

    no se a ver si se arregla esto

---
+++ myapp/urls.py
@@ -0,0 +1,14 @@
+
+
+from django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('catalogo/', views.catalogo, name="catalogoMas"),
+    path('registro/', views.registro, name="registro"),
+    path('inicioSesion/', views.signin, name="inicioSesion"),
+    path('logout/', views.signout, name='logout'),
+    path('registroMascota/', views.registromascota, name="registroMascota"),
+    
+]
