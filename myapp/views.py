91c751ff45e604d4b069c06b99bb4138d602b8da
Fhight <74805803+Fhight@users.noreply.github.com>
2022-10-12 22:17:02 -0500

    Prueba

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import CrearUsuario, InicioSesion, RegistroForm, buscadorContactoMensajeria, enviarMensaje
-from .models import Usuario
 from django.contrib.auth.models import User 
 from django.views.generic import CreateView
 from django.contrib.auth import login, logout, authenticate
